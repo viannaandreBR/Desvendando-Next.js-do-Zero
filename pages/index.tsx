2696b64e4717af68439769854ba5a7156757a7b4
viannaandreBR <viannaandresouza@gmail.com>
2022-02-20 00:00:51 -0300

    5 files added, 4 deleted

---
+++ pages/index.tsx
@@ -0,0 +1,40 @@
+import { GetServerSideProps } from 'next';
+import { useEffect, useState } from 'react';
+
+export default function Home({ repositories }) {
+  // const [repositories, setRepositories] = useState<string[]>([]);
+
+  // useEffect(() => {
+  //   fetch('https://api.github.com/users/viannaandrebr/repos')
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       const repositoryNames = data.map((item) => item.name);
+
+  //       setRepositories(repositoryNames);
+  //     });
+  // }, []);
+
+  return (
+    <ul>
+      {repositories.map((repo) => (
+        <li key={repo}> {repo} </li>
+      ))}
+    </ul>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await fetch(
+    'https://api.github.com/users/viannaandrebr/repos'
+  );
+
+  const data = await response.json();
+
+  const repositoryNames = data.map((item) => item.name);
+
+  return {
+    props: {
+      repositories: repositoryNames,
+    },
+  };
+};
